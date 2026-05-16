bcf4d98b7ebdec02afa50813c0786ec635d9728a
sarcia3 <sara.bartman.05@gmail.com>
2026-05-16 14:21:47 +0200

    Descriptive names for hitpoints

--- src/main/java/org/tcs/model/Creature.java
+++ src/main/java/org/tcs/model/Creature.java
@@ -5,17 +5,21 @@ import java.util.Map;
 
 /** Player, monster, summon etc. Basically anything that exists, has hp and takes actions */
 public class Creature implements HasInitiative {
-  int hp;
+
+  int hitPoints = 0;
+  int hitPointMaximum;
+  int temporaryHitPoints = 0;
+
   String name; // effectively final for the time being, but this might change in the future
   Map<Ability, Integer> abilityScores = new EnumMap<>(Ability.class);
 
   /** movement speed in feet */
   int movementSpeed = 0;
 
-  Creature(String name, int hp, int movementSpeed) {
+  Creature(String name, int hitPointMaximum, int movementSpeed) {
     // this constructor should be deleted later. This is for the minimal working example
     this.name = name;
-    this.hp = hp;
+    this.hitPointMaximum = this.hitPoints = hitPointMaximum;
     movementSpeed = 30;
     for (Ability ability : Ability.values()) {
       abilityScores.put(ability, 10);
