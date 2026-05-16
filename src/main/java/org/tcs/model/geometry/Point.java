a19bfc06f62cc7cdf2fdeab8333e432db83da21e
sarcia3 <sara.bartman.05@gmail.com>
2026-05-16 16:45:30 +0200

    Add doc comment to Point.java

--- src/main/java/org/tcs/model/geometry/Point.java
+++ src/main/java/org/tcs/model/geometry/Point.java
@@ -1,3 +1,9 @@
 package org.tcs.model.geometry;
 
+/**
+ * Point is meant to store the point in space. Different geometries will result in different point
+ * implementations for example 2,5D will store 3 coordinates, while normal grid will store only 2.
+ * More discussion available <a
+ * href="https://github.com/sarcia3/5e-combat-companion/pull/2#discussion_r3252825300">here</a>.
+ */
 public interface Point {}
