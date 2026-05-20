cdadeba606f49f53df1fba2f027b1100b6ee62ca
sarcia3 <sara.bartman.05@gmail.com>
2026-05-20 21:12:15 +0200

    Update creature to use the RandomDiceRoller by default

--- src/main/java/org/tcs/model/Creature.java
+++ src/main/java/org/tcs/model/Creature.java
@@ -2,6 +2,8 @@ package org.tcs.model;
 
 import java.util.EnumMap;
 import java.util.Map;
+import org.tcs.model.dice.DiceRoller;
+import org.tcs.model.dice.RandomDiceRoller;
 
 /** Player, monster, summon etc. Basically anything that exists, has hp and takes actions */
 public class Creature implements HasInitiative {
@@ -9,6 +11,7 @@ public class Creature implements HasInitiative {
   int hitPoints = 0;
   int hitPointMaximum;
   int temporaryHitPoints = 0;
+  DiceRoller diceRoller;
 
   String name; // effectively final for the time being, but this might change in the future
   Map<Ability, Integer> abilityScores = new EnumMap<>(Ability.class);
@@ -16,19 +19,24 @@ public class Creature implements HasInitiative {
   /** movement speed in feet */
   int movementSpeed = 0;
 
+  /** Creates a new creature. Random dice rolling by default. */
   Creature(String name, int hitPointMaximum, int movementSpeed) {
+    this(name, hitPointMaximum, movementSpeed, new RandomDiceRoller());
+  }
+
+  Creature(String name, int hitPointMaximum, int movementSpeed, DiceRoller diceRoller) {
     // this constructor should be deleted later. This is for the minimal working example
     this.name = name;
     this.hitPointMaximum = this.hitPoints = hitPointMaximum;
     this.movementSpeed = movementSpeed;
-    for (Ability ability : Ability.values()) {
-      abilityScores.put(ability, 10);
-    }
+
+    for (Ability ability : Ability.values()) abilityScores.put(ability, 10);
+
+    this.diceRoller = new RandomDiceRoller();
   }
 
   @Override
   public int generateInitiative() {
-    return 20;
-    // TODO: Create randomness generator class in the future
+    return diceRoller.roll(20); // TODO add DEX modifier to the roll
   }
 }
