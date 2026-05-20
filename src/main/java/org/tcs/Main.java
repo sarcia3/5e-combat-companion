ca3e8a61f31aca4f546ed0461cb29671e6d381f0
SmakoszJan <margos.michal@gmail.com>
2026-05-20 19:52:40 +0200

    An ugly green rectangle to get things started

--- src/main/java/org/tcs/Main.java
+++ src/main/java/org/tcs/Main.java
@@ -1,7 +1,15 @@
 package org.tcs;
 
-public class Main {
-  static void main() {
-    System.out.println("Hello World!");
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.tcs.ui.PlayView;
+import org.tcs.ui.ViewModel;
+
+public class Main extends Application {
+  @Override
+  public void start(Stage primaryStage) {
+    primaryStage.setScene(PlayView.scene(new ViewModel()));
+    primaryStage.setTitle("Drageons&Dungons 5e Combat Companion");
+    primaryStage.show();
   }
 }
