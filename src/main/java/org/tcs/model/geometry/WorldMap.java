bc2d614d201e11009e863a1a099f75cdcdfcf059
dartsakratus <46405253+dartsakratus@users.noreply.github.com>
2026-05-20 19:23:34 +0200

    Created WorldMap interface.

---
+++ src/main/java/org/tcs/model/geometry/WorldMap.java
@@ -0,0 +1,19 @@
+﻿package org.tcs.model.geometry;
+
+public interface WorldMap {
+  float getDistance(Point start, Point end);
+
+  RealPoint pointToRealPoint(Point point);
+
+  Point realPointToPoint(RealPoint realPoint);
+
+  /**
+   * @return True if the point was not occupied yet and false otherwise.
+   */
+  boolean occupyPoint(Point point);
+
+  /**
+   * @return True if the point was occupied and false otherwise.
+   */
+  boolean freePoint(Point point);
+}
