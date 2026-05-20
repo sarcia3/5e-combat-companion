1030d66b283ec8f0c87825d0a5e076169c8f336b
SmakoszJan <margos.michal@gmail.com>
2026-05-21 00:13:31 +0200

    Tile picking now follows the `WorldMap` interface

--- src/main/java/org/tcs/ui/ViewModel.java
+++ src/main/java/org/tcs/ui/ViewModel.java
@@ -1,3 +1,12 @@
 package org.tcs.ui;
 
-public class ViewModel {}
+import org.tcs.model.geometry.Finite2DGrid;
+import org.tcs.model.geometry.WorldMap;
+
+public class ViewModel {
+  private final WorldMap map = new Finite2DGrid(100, 100);
+
+  public WorldMap getMap() {
+    return map;
+  }
+}
