a03da72b35b95d9ac6a1886709f483b3bdb8aada
dartsakratus <46405253+dartsakratus@users.noreply.github.com>
2026-05-20 15:38:11 +0200

    Created structure for Prototype usage and a basic PrototypeWrapper.

---
+++ src/main/java/org/tcs/model/prototypes/Prototype.java
@@ -0,0 +1,7 @@
+﻿package org.tcs.model.prototypes;
+
+import java.io.*;
+
+public interface Prototype extends Serializable {
+    String getName();
+}
---
+++ src/main/java/org/tcs/model/prototypes/PrototypeWrapper.java
@@ -0,0 +1,40 @@
+﻿package org.tcs.model.prototypes;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serial;
+
+public class PrototypeWrapper implements Prototype {
+    String name;
+    transient Prototype realPrototype;
+
+    PrototypeWrapper(){
+        name=null;
+        realPrototype=null;}
+
+    PrototypeWrapper(String name)
+    {
+        this.name=name;
+        realPrototype=PrototypesLibrary.get(name);
+    }
+
+    Prototype getPrototype(){
+        return realPrototype;
+    }
+    @Serial
+    private void writeObject(ObjectOutputStream oos) throws IOException {
+        name=realPrototype.getName();
+        oos.defaultWriteObject();
+    }
+    @Serial
+    private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
+        ois.defaultReadObject();
+
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
