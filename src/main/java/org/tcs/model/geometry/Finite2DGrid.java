8e7527f27ddc65f6d66bb8ab218c8145a25447d5
dartsakratus <46405253+dartsakratus@users.noreply.github.com>
2026-05-20 22:01:43 +0200

    Changed Point2D to GridPoint2D.

--- src/main/java/org/tcs/model/geometry/Finite2DGrid.java
+++ src/main/java/org/tcs/model/geometry/Finite2DGrid.java
@@ -5,7 +5,7 @@ import javafx.util.Pair;
 
 public class Finite2DGrid implements WorldMap {
   private final int width, height;
-  private final Set<Point2D> occupiedPoints;
+  private final Set<GridPoint2D> occupiedPoints;
 
   public Finite2DGrid(int width, int height) {
     this.width = width;
@@ -15,23 +15,23 @@ public class Finite2DGrid implements WorldMap {
 
   @Override
   public double getDistance(Point start, Point end) {
-    Point2D start2D = (Point2D) start;
-    Point2D end2D = (Point2D) end;
+    GridPoint2D start2D = (GridPoint2D) start;
+    GridPoint2D end2D = (GridPoint2D) end;
     // For now, it runs Dijkstra, but we should replace it with A* later.
-    Map<Point2D, Double> distances = new HashMap<>();
-    Queue<Pair<Point2D, Double>> queue = new PriorityQueue<>(Comparator.comparing(Pair::getValue));
-    queue.add(new Pair<Point2D, Double>(start2D, 0.));
+    Map<GridPoint2D, Double> distances = new HashMap<>();
+    Queue<Pair<GridPoint2D, Double>> queue = new PriorityQueue<>(Comparator.comparing(Pair::getValue));
+    queue.add(new Pair<GridPoint2D, Double>(start2D, 0.));
     while (!queue.isEmpty() && !distances.containsKey(end2D)) {
-      Pair<Point2D, Double> current = queue.remove();
+      Pair<GridPoint2D, Double> current = queue.remove();
       if (distances.containsKey(current.getKey())) continue;
       else distances.put(current.getKey(), current.getValue());
       for (int i = -1; i <= 1; i++)
         for (int j = -1; j <= 1; j++)
           if (i != 0 || j != 0) {
-            Point2D next = new Point2D(current.getKey().x + i, current.getKey().y + j);
+            GridPoint2D next = new GridPoint2D(current.getKey().x + i, current.getKey().y + j);
             if (!distances.containsKey(next) && checkIfAccessible(next)) {
               queue.add(
-                  new Pair<Point2D, Double>(next, current.getValue() + (i * j > 0 ? 1.5 : 1.)));
+                  new Pair<GridPoint2D, Double>(next, current.getValue() + (i * j > 0 ? 1.5 : 1.)));
             }
           }
     }
@@ -41,23 +41,23 @@ public class Finite2DGrid implements WorldMap {
 
   @Override
   public RealPoint pointToRealPoint(Point point) {
-    if (!(point instanceof Point2D(int x, int y))) throw new ClassCastException();
+    if (!(point instanceof GridPoint2D(int x, int y))) throw new ClassCastException();
     return new RealPoint(x + 0.5, y + 0.5);
   }
 
   @Override
   public Point realPointToPoint(RealPoint realPoint) {
-    return new Point2D((int) Math.floor(realPoint.x()), (int) Math.floor(realPoint.y()));
+    return new GridPoint2D((int) Math.floor(realPoint.x()), (int) Math.floor(realPoint.y()));
   }
 
   @Override
   public boolean occupyPoint(Point point) {
-    return occupiedPoints.add((Point2D) point);
+    return occupiedPoints.add((GridPoint2D) point);
   }
 
   @Override
   public boolean freePoint(Point point) {
-    return occupiedPoints.remove((Point2D) point);
+    return occupiedPoints.remove((GridPoint2D) point);
   }
 
   @Override
@@ -65,19 +65,19 @@ public class Finite2DGrid implements WorldMap {
     return 1.;
   }
 
-  private record Point2D(int x, int y) implements Point {
+  private record GridPoint2D(int x, int y) implements Point {
 
     @Override
     public boolean equals(Object obj) {
-      if (!(obj instanceof Point2D(int x1, int y1))) return false;
+      if (!(obj instanceof GridPoint2D(int x1, int y1))) return false;
       return x1 == x && y1 == y;
     }
   }
 
-  private boolean checkIfAccessible(Point2D point2D) {
-    boolean xInBounds = point2D.x < width && point2D.x >= 0;
-    boolean yInBounds = point2D.y < height && point2D.y >= 0;
-    boolean occupied = occupiedPoints.contains(point2D);
+  private boolean checkIfAccessible(GridPoint2D gridPoint2D) {
+    boolean xInBounds = gridPoint2D.x < width && gridPoint2D.x >= 0;
+    boolean yInBounds = gridPoint2D.y < height && gridPoint2D.y >= 0;
+    boolean occupied = occupiedPoints.contains(gridPoint2D);
     return xInBounds && yInBounds && (!occupied);
   }
 }
