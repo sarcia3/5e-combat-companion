832defa6b3d222c2bb3768f18290461af1193a05
dartsakratus <46405253+dartsakratus@users.noreply.github.com>
2026-05-20 20:54:51 +0200

    Equality methods for RealPoint

--- src/main/java/org/tcs/model/geometry/RealPoint.java
+++ src/main/java/org/tcs/model/geometry/RealPoint.java
@@ -1,3 +1,15 @@
 package org.tcs.model.geometry;
 
-public record RealPoint(double x, double y) {}
+public record RealPoint(double x, double y) {
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof RealPoint(double x1, double y1)))
+            return false;
+        return (x1 ==x) && (y1 ==y);
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) Math.floor(x*1e6+y);
+    }
+}
