c74d94946a465122c6ea2af087d0bc9e35bd7188
SmakoszJan <margos.michal@gmail.com>
2026-05-21 16:48:06 +0200

    Adding decorations

--- src/main/java/org/tcs/ui/Decoration.java
+++ src/main/java/org/tcs/ui/Decoration.java
@@ -1,5 +1,6 @@
 package org.tcs.ui;
 
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import org.tcs.model.geometry.RealPoint;
 
@@ -22,5 +23,9 @@ public class Decoration {
 
   public Image getImage() {
     return image;
+  }
+
+  public void draw(GraphicsContext gc) {
+    gc.drawImage(image, position.x() - 60, position.y() - 60, 120, 120);
   }
 }
