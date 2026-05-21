5905b8cdc903d28b04881d2a2167007c88de6186
SmakoszJan <margos.michal@gmail.com>
2026-05-21 10:39:14 +0200

    Mouse button detection

--- src/main/java/org/tcs/ui/Assets.java
+++ src/main/java/org/tcs/ui/Assets.java
@@ -6,6 +6,6 @@ public class Assets {
   public static Image truck;
 
   public static void init() {
-    truck = new Image("/Truck_Red_Front.png", MapView.PX_PER_FT, MapView.PX_PER_FT, true, true);
+    truck = new Image("/Truck_Red_Front.png");
   }
 }
