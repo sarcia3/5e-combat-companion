525dfda7e8ae32ebd0a2964089b19bdd6cfaa8f7
Krzysztof Salata <46405253+dartsakratus@users.noreply.github.com>
2026-05-21 11:48:35 +0200

    Added mathematical operations for RealPoint and fixed doc for WorldMap. (#6)
    * Added mathematical operations for RealPoint and fixed doc for WorldMap.
    
    * Simplified docs.

--- src/main/java/org/tcs/model/geometry/RealPoint.java
+++ src/main/java/org/tcs/model/geometry/RealPoint.java
@@ -1,3 +1,15 @@
 package org.tcs.model.geometry;
 
-public record RealPoint(double x, double y) {}
+public record RealPoint(double x, double y) {
+  RealPoint add(RealPoint other) {
+    return new RealPoint(this.x() + other.x(), this.y() + other.y());
+  }
+
+  RealPoint multiply(Double multiplier) {
+    return new RealPoint(multiplier * this.x(), multiplier * this.y());
+  }
+
+  RealPoint divide(Double dividend) {
+    return new RealPoint(this.x() / dividend, this.y() / dividend);
+  }
+}
