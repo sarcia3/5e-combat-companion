f9a4e421eeb42e99cc8b1005a1ec434ed0e0d724
dartsakratus <46405253+dartsakratus@users.noreply.github.com>
2026-05-22 17:04:29 +0200

    Added maxMovementSpeed.

--- src/main/java/org/tcs/model/Creature.java
+++ src/main/java/org/tcs/model/Creature.java
@@ -18,6 +18,7 @@ public class Creature implements HasInitiative, HasHitPoints {
   Map<Ability, Integer> abilityScores = new EnumMap<>(Ability.class);
 
   /** movement speed in feet */
+  double maxMovementSpeed=0;
   double movementSpeed = 0;
 
   /** Creates a new creature. Random dice rolling by default. */
