d37a13fea7fcbba13d7adb0b8e33fe1777332216
dartsakratus <46405253+dartsakratus@users.noreply.github.com>
2026-05-22 11:38:19 +0200

    Moved the calculation of distance to multiple points to WorldMap.

--- src/main/java/org/tcs/model/State.java
+++ src/main/java/org/tcs/model/State.java
@@ -63,11 +63,12 @@ public class State {
    */
   // TODO add an option to ignore obstacles
   public Collection<Creature> getCreaturesWithinDistance(Point point, Double distance) {
+    List<Point> pointsInDistance = new ArrayList<>();
+    for (Pair<Point, Double> pair : worldMap.getDistance(point, creaturePositions.values()))
+      if (pair.second() <= distance) pointsInDistance.add(pair.first());
     List<Creature> list = new ArrayList<>();
-    for (Map.Entry<Creature, Point> entry : creaturePositions.entrySet()) {
-      System.out.println(worldMap.getDistance(point, entry.getValue()));
-      if (worldMap.getDistance(point, entry.getValue()) <= distance) list.add(entry.getKey());
-    }
+    for (Map.Entry<Creature, Point> entry : creaturePositions.entrySet())
+      if (pointsInDistance.contains(entry.getValue())) list.add(entry.getKey());
     return list;
   }
 
