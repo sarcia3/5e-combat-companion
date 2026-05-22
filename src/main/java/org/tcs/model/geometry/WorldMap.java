65a5af39705cae1ff7a7e67f29e44aff75090b50
dartsakratus <46405253+dartsakratus@users.noreply.github.com>
2026-05-22 11:38:19 +0200

    Moved the calculation of distance to multiple points to WorldMap.

--- src/main/java/org/tcs/model/geometry/WorldMap.java
+++ src/main/java/org/tcs/model/geometry/WorldMap.java
@@ -1,18 +1,27 @@
 package org.tcs.model.geometry;
 
+import java.util.Collection;
+import org.tcs.model.util.Pair;
+
 public interface WorldMap {
   /**
    * @return The distance between two points or Double.POSITIVE_INFINITY if there is no path between
    *     them or one/both of them are not part of the map.
    */
-  double getDistance(Point start, Point end);
+  double getDistance(Point start, Point target);
+
+  /**
+   * @return A collection of distances between the starting point and those of target points that
+   *     can be reached ordered ascendingly by the distance.
+   */
+  Collection<Pair<Point, Double>> getDistance(Point start, Collection<Point> targets);
 
   RealPoint pointToRealPoint(Point point);
 
   Point realPointToPoint(RealPoint realPoint);
 
   /**
-   * Occupied a point if it was previously occupied.
+   * Occupies a point if it was previously occupied.
    *
    * @return False it the given point is already occupied or is not part of the map.
    */
---
+++ src/main/java/org/tcs/model/util/Pair.java
@@ -0,0 +1,3 @@
+package org.tcs.model.util;
+
+public record Pair<K, L>(K first, L second) {}
