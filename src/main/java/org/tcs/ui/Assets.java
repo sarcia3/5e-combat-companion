be112fff5911be5318d1dd72adf317ad72c9588d
SmakoszJan <margos.michal@gmail.com>
2026-05-23 12:27:38 +0200

    More images for testing

--- src/main/java/org/tcs/ui/Assets.java
+++ src/main/java/org/tcs/ui/Assets.java
@@ -15,5 +15,12 @@ public class Assets {
     images.put("queen", new Image("/w_Queen.png"));
     images.put("rook", new Image("/w_Rook.png"));
     images.put("king", new Image("/w_King.png"));
+    images.put("cinema", new Image("/Building_Cinema.png"));
+    images.put("trashcan", new Image("/Decor_TrashCan.png"));
+    images.put("tree 1", new Image("/Tree1.png"));
+    images.put("tree 2", new Image("/Tree2.png"));
+    images.put("tree 3", new Image("/Tree3.png"));
+    images.put("tree 4", new Image("/Tree4.png"));
+    images.put("tree 5", new Image("/Tree5.png"));
   }
 }
