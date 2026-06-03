4edb731b2a5dbd1ea8ed593fe281d1fd46fbee28
dartsakratus <46405253+dartsakratus@users.noreply.github.com>
2026-06-03 16:17:25 +0200

    Refactoring pt 1

--- src/main/java/org/tcs/model/geometry/Finite2DGrid.java
+++ src/main/java/org/tcs/model/geometry/Finite2DGrid.java
@@ -1,7 +1,6 @@
 package org.tcs.model.geometry;
 
 import java.util.*;
-import java.util.List;
 import org.tcs.model.util.Pair;
 
 public class Finite2DGrid implements WorldMap {
@@ -23,64 +22,42 @@ public class Finite2DGrid implements WorldMap {
    * sqrt(2).
    */
   @Override
-  public double getDistance(Point start, Point target, Collection<OccupyReason> collisionLayers) {
+  public double getDistance(Point start, Point target) {
     GridPoint2D start2D = (GridPoint2D) start;
     GridPoint2D target2D = (GridPoint2D) target;
 
-    // Ideologically this is an implementation of A* with function h(x) being the distance between
-    // the points if there were no obstacles. One can see that it satisfies h(x)<=d(x,y)+h(y) so it
-    // always finds the shortest path (see
-    // https://en.wikipedia.org/wiki/A*_search_algorithm#Description).
-    // However, for the sake of cleaner implementation we add the difference between h(x) and h(y)
-    // to the edge weight and run Dijkstra algorithm to obtain the result.
-
-    Map<GridPoint2D, Double> distances = new TreeMap<>();
+    // For now, it runs Dijkstra, but we should replace it with A* later.
+    Map<GridPoint2D, Double> distances = new HashMap<>();
     Queue<Pair<GridPoint2D, Double>> queue =
         new PriorityQueue<>(Comparator.comparing(Pair::second));
     queue.add(new Pair<GridPoint2D, Double>(start2D, 0.));
-
     while (!queue.isEmpty() && !distances.containsKey(target2D)) {
       Pair<GridPoint2D, Double> current = queue.remove();
-
       // If we've already been in this point do nothing
       if (distances.containsKey(current.first())) continue;
-
-      // If this point is occupied or outside the map do nothing
-      if (!checkInBounds(current.first())) continue;
-      OccupyReason occupyReason = getOccupyReason(current.first());
-      if (occupyReason != null && !collisionLayers.contains(occupyReason)) continue;
-
       distances.put(current.first(), current.second());
-
+      // If this point is occupied or outside the map do nothing
+      if (!checkAccessible(current.first())) continue;
       // Otherwise go to neighbours
       for (Pair<GridPoint2D, Double> next : getNeighbours(current.first())) {
         if (!distances.containsKey(next.first())) {
-          double weight = next.second();
-          weight +=
-              primitiveDistance(next.first(), target2D)
-                  - primitiveDistance(current.first(), target2D);
-
-          queue.add(new Pair<GridPoint2D, Double>(next.first(), current.second() + weight));
+          queue.add(new Pair<GridPoint2D, Double>(next.first(), next.second() + current.second()));
         }
       }
     }
 
-    if (distances.containsKey(target2D))
-      return distances.get(target2D) + primitiveDistance(start2D, target2D);
-    return Double.POSITIVE_INFINITY;
+    return distances.getOrDefault(target2D, Double.POSITIVE_INFINITY);
   }
 
   @Override
-  public Collection<Pair<Point, Double>> getDistances(
-      Point start, Collection<Point> targets, Collection<OccupyReason> collisionLayers) {
-
+  public Collection<Pair<Point, Double>> getDistances(Point start, Collection<Point> targets) {
     List<Pair<Point, Double>> list = new ArrayList<>();
     GridPoint2D start2D = (GridPoint2D) start;
-    Map<GridPoint2D, Double> distances = new TreeMap<>();
+    Map<GridPoint2D, Double> distances = new HashMap<>();
     Queue<Pair<GridPoint2D, Double>> queue =
         new PriorityQueue<>(Comparator.comparing(Pair::second));
-    queue.add(new Pair<GridPoint2D, Double>(start2D, 0.));
 
+    queue.add(new Pair<GridPoint2D, Double>(start2D, 0.));
     while (!queue.isEmpty() && list.size() < targets.size()) {
 
       Pair<GridPoint2D, Double> current = queue.remove();
@@ -92,9 +69,7 @@ public class Finite2DGrid implements WorldMap {
         list.add(new Pair<Point, Double>(current.first(), current.second()));
 
       // If this point is occupied or outside the map do nothing
-      if (!checkInBounds(current.first())) continue;
-      OccupyReason occupyReason = getOccupyReason(current.first());
-      if (occupyReason != null && !collisionLayers.contains(occupyReason)) continue;
+      if (!checkAccessible(current.first())) continue;
 
       // Otherwise go to neighbours
       for (Pair<GridPoint2D, Double> next : getNeighbours(current.first())) {
@@ -147,18 +122,16 @@ public class Finite2DGrid implements WorldMap {
     return true;
   }
 
-  private record GridPoint2D(int x, int y) implements Point, Comparable<GridPoint2D> {
-    @Override
-    public int compareTo(GridPoint2D o) {
-      if (x == o.x()) return Integer.compare(y, o.y());
-      return Integer.compare(x, o.x());
-    }
-  }
+  private record GridPoint2D(int x, int y) implements Point {}
 
   private boolean checkInBounds(GridPoint2D gridPoint2D) {
     boolean xInBounds = gridPoint2D.x < width && gridPoint2D.x >= 0;
     boolean yInBounds = gridPoint2D.y < height && gridPoint2D.y >= 0;
     return xInBounds && yInBounds;
+  }
+
+  private boolean checkAccessible(GridPoint2D gridPoint2D) {
+    return checkInBounds(gridPoint2D) && !isPointOccupied(gridPoint2D);
   }
 
   /**
@@ -174,15 +147,5 @@ public class Finite2DGrid implements WorldMap {
           if (checkInBounds(prospect)) list.add(new Pair<>(prospect, i * j > 0 ? 1.42 : 1));
         }
     return list;
-  }
-
-  /**
-   * @return The distance of the shortest path, assuming that there are no obstacles.
-   */
-  private double primitiveDistance(GridPoint2D start, GridPoint2D target) {
-    int x = Math.abs(start.x - target.x);
-    int y = Math.abs(start.y - target.y);
-
-    return Math.max(x, y) + Math.min(x, y) * 0.42;
   }
 }
