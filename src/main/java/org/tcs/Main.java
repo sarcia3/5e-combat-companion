03254d1e9cc43894815c82a1785630dcce8da050
SmakoszJan <margos.michal@gmail.com>
2026-06-04 19:02:41 +0200

    Listing weapons & attacking, to an extent

--- src/main/java/org/tcs/Main.java
+++ src/main/java/org/tcs/Main.java
@@ -2,8 +2,11 @@ package org.tcs;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.tcs.model.Ability;
 import org.tcs.model.Creature;
+import org.tcs.model.Damage;
 import org.tcs.model.State;
+import org.tcs.model.equipment.Weapon;
 import org.tcs.model.geometry.Finite2DGrid;
 import org.tcs.model.geometry.RealPoint;
 import org.tcs.ui.Assets;
@@ -16,8 +19,9 @@ public class Main extends Application {
     Assets.init();
     var map = new Finite2DGrid(100, 100);
     var state = new State(map);
-    state.addCreature(
-        new Creature("Test 1", map.realPointToPoint(new RealPoint(1.0, 1.0)), 10, 20));
+    var creature = new Creature("Test 1", map.realPointToPoint(new RealPoint(1.0, 1.0)), 10, 20);
+    creature.addWeapon(new Weapon("aaa", 1, Ability.CHA, Damage.Type.ACID));
+    state.addCreature(creature);
     state.addCreature(
         new Creature("Test 2", map.realPointToPoint(new RealPoint(3.0, 3.0)), 10, 20));
     primaryStage.setScene(new PlayView(new ViewModel(state), primaryStage));
--- src/main/java/org/tcs/ui/CreatureView.java
+++ src/main/java/org/tcs/ui/CreatureView.java
@@ -3,26 +3,61 @@ package org.tcs.ui;
 import java.util.Map;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.ListChangeListener;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import org.tcs.model.Creature;
+import org.tcs.model.equipment.Weapon;
 
 public class CreatureView extends VBox {
-  private final ObjectProperty<Creature> creature = new SimpleObjectProperty<>();
+  private enum Nav {
+    All,
+    Weapons
+  }
 
-  public CreatureView(Map<Creature, Image> creatureImages) {
+  private final ObjectProperty<Creature> creature = new SimpleObjectProperty<>();
+  private final ObjectProperty<Nav> nav = new SimpleObjectProperty<>(Nav.All);
+
+  public CreatureView(Map<Creature, Image> creatureImages, ViewModel model) {
+    creature.addListener(_ -> model.loadCreature(creature.get()));
+
     var portrait = new ImageView();
     portrait.setFitHeight(256.0);
     portrait.setPreserveRatio(true);
     portrait
         .imageProperty()
         .bind(creature.map(key -> creatureImages.getOrDefault(key, Assets.PLACEHOLDER)));
-    getChildren().add(portrait);
 
+    var attack = new Button("Attack");
+    attack.setOnAction(_ -> nav.set(Nav.Weapons));
+
+    var topLevel = new VBox();
+    topLevel.getChildren().add(attack);
+    topLevel.setAlignment(Pos.CENTER);
+    topLevel.visibleProperty().bind(nav.isEqualTo(Nav.All));
+    topLevel.managedProperty().bind(nav.isEqualTo(Nav.All));
+
+    var weapons = new VBox();
+    model
+        .weaponsProperty()
+        .addListener(
+            (ListChangeListener<? super Weapon>)
+                _ -> {
+                  weapons.getChildren().clear();
+                  for (Weapon weapon : model.weaponsProperty()) {
+                    var btn = new Button(weapon.toString());
+                    weapons.getChildren().add(btn);
+                  }
+                });
+    weapons.visibleProperty().bind(nav.isEqualTo(Nav.Weapons));
+    weapons.managedProperty().bind(nav.isEqualTo(Nav.Weapons));
+
+    getChildren().addAll(portrait, topLevel, weapons);
     setMaxWidth(320.0);
     setAlignment(Pos.TOP_CENTER);
     setBackground(Background.fill(Color.WHITE));
