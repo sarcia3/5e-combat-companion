e0b7edb904dfad083f900e92c1eb5f34dc1df6a8
sarcia3 <sara.bartman.05@gmail.com>
2026-06-09 22:18:55 +0200

    Add range to weapon mode

--- src/main/java/org/tcs/model/equipment/WeaponsLibrary.java
+++ src/main/java/org/tcs/model/equipment/WeaponsLibrary.java
@@ -12,69 +12,69 @@ public class WeaponsLibrary {
 
   static {
     // See https://5e.tools/variantrules.html#unarmed%20strike_xphb
-    add(new Weapon("Fist", "1d1 BLUDGEONING", Ability.STR, false));
+    add(new Weapon("Fist", "1d1 BLUDGEONING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
 
     // Simple Melee Weapons
-    add(new Weapon("Club", "1d4 BLUDGEONING", Ability.STR, false));
-    add(new Weapon("Sickle", "1d4 SLASHING", Ability.STR, false));
-    add(new Weapon("Light Hammer", "1d4 BLUDGEONING", Ability.STR, false));
-    add(new Weapon("Mace", "1d6 BLUDGEONING", Ability.STR, false));
-    add(new Weapon("Quarterstaff", "1d6 BLUDGEONING", Ability.STR, false));
-    add(new Weapon("Spear", "1d6 PIERCING", Ability.STR, false));
-    add(new Weapon("Javelin", "1d6 PIERCING", Ability.STR, false));
+    add(new Weapon("Club", "1d4 BLUDGEONING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("Sickle", "1d4 SLASHING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("Light Hammer", "1d4 BLUDGEONING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("Mace", "1d6 BLUDGEONING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("Quarterstaff", "1d6 BLUDGEONING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("Spear", "1d6 PIERCING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("Javelin", "1d6 PIERCING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
 
     // Martial Melee Weapons
-    add(new Weapon("Battleaxe", "1d8 SLASHING", Ability.STR, false));
-    add(new Weapon("Flail", "1d8 BLUDGEONING", Ability.STR, false));
-    add(new Weapon("Morningstar", "1d8 PIERCING", Ability.STR, false));
-    add(new Weapon("War Pick", "1d8 PIERCING", Ability.STR, false));
-    add(new Weapon("Warhammer", "1d8 BLUDGEONING", Ability.STR, false));
-    add(new Weapon("Trident", "1d6 PIERCING", Ability.STR, false));
+    add(new Weapon("Battleaxe", "1d8 SLASHING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("Flail", "1d8 BLUDGEONING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("Morningstar", "1d8 PIERCING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("War Pick", "1d8 PIERCING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("Warhammer", "1d8 BLUDGEONING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("Trident", "1d6 PIERCING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
 
     // Finesse Martial Weapons (DEX or STR)
     add(
         new Weapon(
             "Shortsword",
             List.of(
-                new Weapon.Mode("1d6 PIERCING", Ability.DEX, false, null),
-                new Weapon.Mode("1d6 PIERCING", Ability.STR, false, null))));
+                new Weapon.Mode("1d6 PIERCING", Ability.DEX, false, null, WeaponRange.MELEE_STANDARD),
+                new Weapon.Mode("1d6 PIERCING", Ability.STR, false, null, WeaponRange.MELEE_STANDARD))));
 
     add(
         new Weapon(
             "Scimitar",
             List.of(
-                new Weapon.Mode("1d6 SLASHING", Ability.DEX, false, null),
-                new Weapon.Mode("1d6 SLASHING", Ability.STR, false, null))));
+                new Weapon.Mode("1d6 SLASHING", Ability.DEX, false, null, WeaponRange.MELEE_STANDARD),
+                new Weapon.Mode("1d6 SLASHING", Ability.STR, false, null, WeaponRange.MELEE_STANDARD))));
 
     add(
         new Weapon(
             "Rapier",
             List.of(
-                new Weapon.Mode("1d8 PIERCING", Ability.DEX, false, null),
-                new Weapon.Mode("1d8 PIERCING", Ability.STR, false, null))));
+                new Weapon.Mode("1d8 PIERCING", Ability.DEX, false, null, WeaponRange.MELEE_STANDARD),
+                new Weapon.Mode("1d8 PIERCING", Ability.STR, false, null, WeaponRange.MELEE_STANDARD))));
 
     add(
         new Weapon(
             "Whip",
             List.of(
-                new Weapon.Mode("1d4 SLASHING", Ability.DEX, false, null),
-                new Weapon.Mode("1d4 SLASHING", Ability.STR, false, null))));
+                new Weapon.Mode("1d4 SLASHING", Ability.DEX, false, null, WeaponRange.MELEE_STANDARD),
+                new Weapon.Mode("1d4 SLASHING", Ability.STR, false, null, WeaponRange.MELEE_STANDARD))));
     add(
         new Weapon(
             "Dagger",
             List.of(
-                new Weapon.Mode("1d4 PIERCING", Ability.DEX, false, null),
-                new Weapon.Mode("1d4 PIERCING", Ability.STR, false, null))));
+                new Weapon.Mode("1d4 PIERCING", Ability.DEX, false, null, WeaponRange.MELEE_STANDARD),
+                new Weapon.Mode("1d4 PIERCING", Ability.STR, false, null, WeaponRange.MELEE_STANDARD))));
     // new Weapon.Mode("1d4 PIERCING", Ability.DEX, true) when we add ranged weapons
 
     // Heavy Martial Melee Weapons (TODO two-handed only)
-    add(new Weapon("Greataxe", "1d12 SLASHING", Ability.STR, false));
-    add(new Weapon("Greatsword", "2d6 SLASHING", Ability.STR, false));
-    add(new Weapon("Maul", "2d6 BLUDGEONING", Ability.STR, false));
-    add(new Weapon("Halberd", "1d10 SLASHING", Ability.STR, false));
-    add(new Weapon("Glaive", "1d10 SLASHING", Ability.STR, false));
-    add(new Weapon("Pike", "1d10 PIERCING", Ability.STR, false));
-    add(new Weapon("Lance", "1d12 PIERCING", Ability.STR, false));
+    add(new Weapon("Greataxe", "1d12 SLASHING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("Greatsword", "2d6 SLASHING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("Maul", "2d6 BLUDGEONING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("Halberd", "1d10 SLASHING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("Glaive", "1d10 SLASHING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("Pike", "1d10 PIERCING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
+    add(new Weapon("Lance", "1d12 PIERCING", Ability.STR, false, WeaponRange.MELEE_STANDARD));
   }
 
   public static Collection<Weapon> getWeapons() {
