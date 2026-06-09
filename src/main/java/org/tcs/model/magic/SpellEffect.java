0e86b521f02a5933729cbee74b4736ef2bb0d49a
sarcia3 <sara.bartman.05@gmail.com>
2026-06-09 19:56:44 +0200

    Fix formatting issue

--- src/main/java/org/tcs/model/magic/SpellEffect.java
+++ src/main/java/org/tcs/model/magic/SpellEffect.java
@@ -5,5 +5,5 @@ import org.tcs.model.Creature;
 import org.tcs.model.State;
 
 public interface SpellEffect {
-  void resolve(State state, Creature caster, Collection<Creature> Targets, SpellLevel slotLevel);
+  void resolve(State state, Creature caster, Collection<Creature> targets, SpellLevel slotLevel);
 }
